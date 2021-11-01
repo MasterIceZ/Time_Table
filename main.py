26f5145242e6fa30bd2bdb8bced0f10767a97317
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-11-01 01:26:28 +0000

    update link

--- main.py
+++ main.py
@@ -12,112 +12,112 @@ def c_d(teach, link):
 
 async def kim():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="EN32102", description=c_d("Somanat/Kimberly", "https://meet.google.com/lookup/aqwjoc5qw4?authuser=0&hs=179"))
+	res = discord.Embed(title="EN32102", description=c_d("Somanat/Kimberly", "https://meet.google.com/lookup/aqwjoc5qw4?authuser=0&hs=179"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def social():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SO32103", description=c_d("Weerarat", "None"))
+	res = discord.Embed(title="SO32103", description=c_d("Weerarat", "None"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def niwpong():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="MA32202", description=c_d("Woratep", "https://us02web.zoom.us/j/6463824790?pwd=ZmRZcU1NT1RaRW12M24wRXRKM2c4UT09"))
+	res = discord.Embed(title="MA32202", description=c_d("Woratep", "https://us02web.zoom.us/j/6463824790?pwd=ZmRZcU1NT1RaRW12M24wRXRKM2c4UT09"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def com():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32182/SC30289", description=c_d("Yuwadee", "None"))
+	res = discord.Embed(title="SC32182/SC30289", description=c_d("Yuwadee", "None"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def bio():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32244", description=c_d("Kwanjai", "https://meet.google.com/lookup/dk4vbsauko"))
+	res = discord.Embed(title="SC32244", description=c_d("Kwanjai", "https://meet.google.com/lookup/dk4vbsauko"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def chem():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32224", description=c_d("Trakool", "https://meet.google.com/lookup/fs2mb4nn6o"))
+	res = discord.Embed(title="SC32224", description=c_d("Trakool", "https://meet.google.com/lookup/fs2mb4nn6o"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def thai():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="TH32102", description=c_d("Sutchai", "https://meet.google.com/lookup/hqv7ctvox3"))
+	res = discord.Embed(title="TH32102", description=c_d("Sutchai", "https://meet.google.com/lookup/hqv7ctvox3"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def art():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="AR32102", description=c_d("Kittisak", "https://meet.google.com/lookup/fmpewevwuz"))
+	res = discord.Embed(title="AR32102", description=c_d("Kittisak", "https://meet.google.com/lookup/fmpewevwuz"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def health():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="HP32102", description=c_d("Chantawat", "None"))
+	res = discord.Embed(title="HP32102", description=c_d("Chantawat", "None"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def parinya():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="MA23112", description=c_d("Parinya", "https://meet.google.com/lookup/ctqza3zspv"))
+	res = discord.Embed(title="MA23112", description=c_d("Parinya", "https://meet.google.com/lookup/ctqza3zspv"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def phy():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32204", description=c_d("Paitoon", "https://meet.google.com/ixf-snzt-jtx"))
+	res = discord.Embed(title="SC32204", description=c_d("Paitoon", "https://meet.google.com/ixf-snzt-jtx"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def history():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SO32104", description=c_d("Nisalai", "https://meet.google.com/lookup/eywcabf4ya"))
+	res = discord.Embed(title="SO32104", description=c_d("Nisalai", "https://meet.google.com/lookup/eywcabf4ya"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def homeroom():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="Homeroom", description=c_d("Trakool/Natapon", "https://meet.google.com/lookup/fs2mb4nn6o"))
+	res = discord.Embed(title="Homeroom", description=c_d("Trakool/Natapon", "https://meet.google.com/lookup/fs2mb4nn6o"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def phy2():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32112", description=c_d("Ornanong", "https://meet.google.com/bgd-dnhk-tvw?authuser=0&hs=179"))
+	res = discord.Embed(title="SC32112", description=c_d("Ornanong", "https://meet.google.com/bgd-dnhk-tvw?authuser=0&hs=179"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def eng():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="EN32202", description=c_d("Waraporn", "https://meet.google.com/lookup/dcxdxsg2bj"))
+	res = discord.Embed(title="EN32202", description=c_d("Waraporn", "https://meet.google.com/lookup/dcxdxsg2bj"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def iot():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32283", description=c_d("Wattana", "https://meet.google.com/nqm-xxko-jcf"))
+	res = discord.Embed(title="SC32283", description=c_d("Wattana", "https://meet.google.com/nqm-xxko-jcf"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def ot():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="OT32102", description=c_d("Amornrat", "None"))
+	res = discord.Embed(title="OT32102", description=c_d("Amornrat", "None"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def eng2():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="OT32102", description=c_d("Somanat", "https://meet.google.com/lookup/a2nnrqnvnc"))
+	res = discord.Embed(title="OT32102", description=c_d("Somanat", "https://meet.google.com/lookup/a2nnrqnvnc"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def soccer():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="HP30204", description=c_d("Ekkaluk", "None"))
+	res = discord.Embed(title="HP30204", description=c_d("Ekkaluk", "None"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def project():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="SC32284", description=c_d("Natapon", "https://meet.google.com/acb-sinx-vkg"))
+	res = discord.Embed(title="SC32284", description=c_d("Natapon", "https://meet.google.com/acb-sinx-vkg"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def guid():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="DA30904", description=c_d("Thanapon", "https://meet.google.com/lookup/ct3fpumjpt"))
+	res = discord.Embed(title="DA30904", description=c_d("Thanapon", "https://meet.google.com/lookup/ct3fpumjpt"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 async def john():
 	chan = client.get_channel(ch)
-	res = discord.Embed(title="EN30020", description=c_d("Waraporn/John", "https://meet.google.com/lookup/htnhbx2gv5"))
+	res = discord.Embed(title="EN30020", description=c_d("Waraporn/John", "https://meet.google.com/lookup/htnhbx2gv5"), color=discord.Color.from_rgb(255, 203, 251))
 	await chan.send(embed=res)
 
 # Monday
