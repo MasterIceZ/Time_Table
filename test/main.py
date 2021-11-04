02ea0be9b5c6eadd30eaefc38c1c3ddfb2df13b5
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-11-04 09:30:52 +0700

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,6 +1,13 @@
 import requests
 import datetime
+import os
 
 res = requests.get('https://Table.aiceaeng.repl.co')
+if str(res) == '<Response [200]>':
+    url = "https://notify-api.line.me/api/notify"
+    token = os.getenv('LINE_TOKEN')
+    headers = {'content-type':'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token}
+    msg = 'Bot is Down! at ' + str(datetime.datetime.now())
+    r = requests.post(url, headers=headers, data={'message':msg})
 print(res)
 print(datetime.datetime.now())
