0064bba3912c627552b2447cdbdfbdb8612367b6
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2021-11-05 12:59:08 +0700

    Update main.py

--- test/main.py
+++ test/main.py
@@ -5,7 +5,7 @@ import pytz
 import linenotif
 
 res = requests.get('https://Table.aiceaeng.repl.co')
-if str(res) != '<Response [200]>':
+if str(res) == '<Response [200]>':
     noti = linenotif.Notify()
     noti.setKey(os.getenv('LINE_TOKEN'))
     noti.send_message('Bot Down!')
