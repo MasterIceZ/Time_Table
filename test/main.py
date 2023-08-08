afccc5efbbe262c1ed2054906a1e641ed07f20d6
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2023-08-08 13:13:37 +0700

    Update main.py

--- test/main.py
+++ test/main.py
@@ -8,6 +8,6 @@ res = requests.get('https://Table.aiceaeng.repl.co')
 if str(res) != '<Response [200]>':
     noti = linenotif.Notify()
     noti.setKey(os.getenv('LINE_TOKEN'))
-    noti.send_message('Bot Down!')
+    # noti.send_message('Bot Down!')
 print(res)
 print(datetime.datetime.now())
